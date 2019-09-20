86b4fa1522eb631324969b11a535abaf065221eb
Peter <peterhahn@Peters-MBP.fios-router.home>
2019-09-20 19:43:00 -0400

    dig

--- components/auditory.js
+++ components/auditory.js
@@ -27,7 +27,7 @@ class Auditory extends React.Component{
 	    	console.log('trash');
 	    	if(typeof window !== 'undefined'){
 		    	var retrievedObject = localStorage.getItem('id');
-		    	console.log(this.props);
+		    	console.log(this.props.artist);
 	    	}
 	    }
 	    else{
